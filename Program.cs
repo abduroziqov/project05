cf993da47e5d30bfa7ea7b15bd2e7fbe547333d3
abdurozikov.kuvonchbek@gmail.com <abdurozikov.kuvonchbek@gmail.com>
2023-03-24 23:34:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculateWeight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          while(true)
+            {
+                Console.Write("Enter your weight: ");
+                float weiht = float.Parse(Console.ReadLine());
+                Console.Write("Enter your height: ");
+                float height = float.Parse(Console.ReadLine());
+                Console.WriteLine($"BMI:{(weiht / (height * height)) * 10000} m/h^2");
+                Console.WriteLine();
+            }
+           
+        }
+    }
+}
